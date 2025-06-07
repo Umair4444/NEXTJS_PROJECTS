93cf89703d5ba256e87634992ac60c204b98a8e9
Umair Ali Khan <100639296+Umair4444@users.noreply.github.com>
2025-06-07 16:28:01 +0500

    minor bug

--- Blooming Narratives/my-app/src/app/page.tsx
+++ Blooming Narratives/my-app/src/app/page.tsx
@@ -1,4 +1,4 @@
-import HeroSection from "@/components/HeroSection";
+import HeroSection from "@/components/HeroSec";
 import FeatureBlogs from "@/components/FeaatureBlogs";
 import { posts } from "@/components/FeatureBlogsData";
 import TrendingTopics from "@/components/TrendingTopics";
