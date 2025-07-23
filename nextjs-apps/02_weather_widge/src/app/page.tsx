3e21a25dc5b1cabde8202ba0e4acd667b2721f28
Umair Ali Khan <100639296+Umair4444@users.noreply.github.com>
2025-07-24 02:48:27 +0500

    weather app

---
+++ nextjs-apps/02_weather_widge/src/app/page.tsx
@@ -0,0 +1,10 @@
+import WeatherCard from "@/components/WeatherCard";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <WeatherCard />
+    </main>
+  );
+}
