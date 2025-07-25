388e650ac2151988c655950b26c5f69c91b30a93
Umair Ali Khan <100639296+Umair4444@users.noreply.github.com>
2025-07-26 04:10:08 +0500

    images updated

--- shopco.store/src/components/(homepage)/Stats.tsx
+++ shopco.store/src/components/(homepage)/Stats.tsx
@@ -5,16 +5,16 @@ const Stats = () => {
     <>
       <div className="bg-black text-white py-12">
         <div className="max-w-7xl mx-auto px-4">
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
-            <div>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 text-center items-center">
+            <div className="pl-8">
               <div className="text-4xl font-bold mb-2">200+</div>
               <div className="text-gray-400">International Brands</div>
             </div>
-            <div className="border-l border-gray-600 pl-8">
+            <div className="md:border-l border-gray-600 pl-8">
               <div className="text-4xl font-bold mb-2">2,000+</div>
               <div className="text-gray-400">High-Quality Products</div>
             </div>
-            <div className="border-l border-gray-600 pl-8">
+            <div className="md:border-l border-gray-600 pl-8">
               <div className="text-4xl font-bold mb-2">30,000+</div>
               <div className="text-gray-400">Happy Customers</div>
             </div>
